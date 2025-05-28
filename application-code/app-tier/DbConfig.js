c79bb3c25efe8384a5f4d9a72bff212126be33fe
Pranav1194 <156526912+Pranav1194@users.noreply.github.com>
2025-05-28 16:02:56 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'oshflois',
-    DB_USER : 'dba',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cst20qcs65pi.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin1234',
+    DB_DATABASE : 'webappdb'
 });
